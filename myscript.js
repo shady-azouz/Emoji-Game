8c2f248e6320dad0bfcd0f842ac105bacd004017
shady-azouz <shady.azouz@gmail.com>
2021-09-12 16:41:32 +0200

    feat: toggle background color

--- myscript.js
+++ myscript.js
@@ -1,14 +1,28 @@
+var numOfImages = 3;
+var background = 0;
+
+function checkList (element, list)
+{
+    let margin = 30;
+    for(let i = 0;i<list.length;i++){
+        if(element < (list[i] + margin) || element > (list[i] - margin))
+            return true;
+    }
+    return false;
+}
+
 function insert(idName1, idName2){
     var left = Math.floor((Math.random() * 150) + 1)+"px";
     var top = Math.floor((Math.random() * 150) + 1)+"px";
-    var r = Math.round(Math.random());
-    var imgName = "";
-    if (r)
-        imgName = "e1.jpg";
-    else
-        imgName = "e3.png";
+    let imgName = randImage();
     insertion(idName1, left, top, imgName);
     insertion(idName2, left, top, imgName);
+}
+
+function randImage() {
+    let r = Math.floor(Math.random() * 3) + 1;
+    let retString = "e" + r +".png";
+    return retString;
 }
 
 function insertion(idName, left, top, imgName)
@@ -31,16 +45,13 @@ function uniqueInsertion(idName)
 {
     var imgDestination = document.getElementById(idName);
     var imgAdded = document.createElement("img");
-    var r = Math.round(Math.random());
-    if(r)
-        var imgName = "e1.jpg";
-    else    
-        var imgName = "e3.png";
+    let imgName = randImage();
     imgAdded.src = imgName;
     imgAdded.width = 30;
     imgAdded.height = 30;
     imgDestination.appendChild(imgAdded);
     imgAdded.style.position = "absolute";
+
     imgAdded.style.top = Math.floor((Math.random() * 150) + 1)+"px";
     imgAdded.style.left = Math.floor((Math.random() * 150) + 1)+"px";
 
@@ -49,20 +60,23 @@ function uniqueInsertion(idName)
 }
 
 function incorrectClick(){
-    document.getElementById("message").innerHTML = "Try Again!";
+    let message = "Level: " + (numOfImages - 2) + " Try Again!";
+    document.getElementById("message").innerHTML = message;
 }
 
 function correctClick() {
     numOfImages ++;
-    document.getElementById("message").innerHTML = "Correct!";
-    newGame();
-}
+    let message = "Level: " + (numOfImages - 2);
+    document.getElementById("message").innerHTML = message;
 
-function newGame() {
     // delete all images
     document.getElementById("gameLeft").innerHTML = "";
     document.getElementById("gameRight").innerHTML = "";
     
+    newGame();
+}
+
+function newGame() {
     for(let i=0; i<numOfImages;i++) {
         insert("gameLeft", "gameRight");
     }
@@ -71,12 +85,24 @@ function newGame() {
         uniqueInsertion("gameLeft");
     else
         uniqueInsertion("gameRight")
+    toggleBackground();
 }
 
-var numOfImages = 3;
+function toggleBackground() {
+    let game = document.getElementById("game");
+    if(background == 0){
+        game.style.backgroundColor = "rgb(170, 50, 48)";
+        background = 1;
+    }
+    else{
+        game.style.backgroundColor = "rgb(237, 183, 72)";
+        background = 0;
+    }
+}
 
 window.onload = function() {
-    for (let i = 0; i < numOfImages; i++) { 
+    document.getElementById("message").innerHTML = "Level: 1";
+    for(let i=0; i<numOfImages;i++) {
         insert("gameLeft", "gameRight");
     }
     var r = Math.round(Math.random());
